4bfcd53f37d6f364a76e5dad505a8505b88d8349
leroy0615 <leroy0615@gmail.com>
2019-06-24 17:23:18 +0000

    last push

--- db/migrate/20190619023450_add_picture_to_photos.rb
+++ db/migrate/20190619023450_add_picture_to_photos.rb
@@ -1,5 +1,7 @@
 class AddPictureToPhotos < ActiveRecord::Migration[5.2]
   def change
-    add_column :caption, :picture
+  	add_column :picture, :caption, :integer
+  	add_column :user_id
+    
   end
 end
